e8fb5c2ecde1e1deefc8d1d690d90098fa75c41e
hassam722 <hassamghori722@gmail.com>
2025-10-03 19:47:46 +0500

    sec

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 import gradio as gr
 import os
 
-model_id = "TheBloke/NexusRaven-V2-13B-GPTQ"
-token = os.environ.get("HF_TOKEN")  # agar repo private ho
+model_id = "TheBloke/NexusRaven-V2-13B-GGUF"
 
-tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=token)  # token specify karo agar private
-model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", trust_remote_code=True, use_auth_token=token)
+tokenizer = AutoTokenizer.from_pretrained(model_id)  # token specify karo agar private
+model = AutoModelForCausalLM.from_pretrained(model_id,model_file = "nexusraven-13b.Q2_K.gguf", device_map="auto", trust_remote_code=True)
 
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 def chat(prompt):
-    return pipe(prompt, max_new_tokens=200, temperature=0.7)[0]["generated_text"]
+    return pipe(prompt, max_new_tokens=200, temperature=0.7)
 
 demo = gr.Interface(fn=chat, inputs="text", outputs="text")
-demo.launch()
+demo.launch(share= True)
