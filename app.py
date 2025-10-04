200684d0ac7b5fa2c3b6de66d911d46e9f62daca
hassam722 <hassamghori722@gmail.com>
2025-10-04 08:34:28 +0500

    commit

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 import gradio as gr
 import os
 
-model_id = "TheBloke/NexusRaven-V2-13B-GGUF"
+model_id = "Nexusflow/NexusRaven-V2-13B"
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)  # token specify karo agar private
-model = AutoModelForCausalLM.from_pretrained(model_id,model_file = "nexusraven-13b.Q2_K.gguf", device_map="auto", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", trust_remote_code=True)
 
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
